62f83234e8f3b09ac07a03b60aca47fc406d521b
bismarckNix <ludocryptwt@proton.me>
2026-09-11 17:16:50 +0300

    changed clean amount

--- nixos/modules/nh.nix
+++ nixos/modules/nh.nix
@@ -2,7 +2,7 @@
   programs.nh = {
     enable = true;
     clean.enable = true;
-    clean.extraArgs = "--keep-since 5d --keep 10";
+    clean.extraArgs = "--keep-since 5d --keep 5";
     flake = "/home/${user}/nix-config";
   };
 }
